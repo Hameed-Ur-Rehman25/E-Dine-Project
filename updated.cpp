05bb7cca136523c61f92f5715501746d01407dbd
Hameed-Ur-Rehman25 <hameedkhan944873@gmail.com>
2023-11-30 13:44:28 +0500

    modified:   updated.cpp

--- updated.cpp
+++ updated.cpp
@@ -13,5 +13,45 @@ inline void title();
 
 int main()
 {
-
+    int option;
+    system("cls");
+    // cout << "\t\t\t\t\t\t\t\t\t*****WELCOME TO E-DINE CAFE*****\n\n";
+    title("WELCOME TO E-DINE CAFE");
+    cout << "\t<1> MANAGER\n\n\t<2> CUSTOMER\n\n\t<3> KITCHEN\n\n\tEnter any key to exit...";
+    cout << "\n\n\t\tChoose any one option: ";
+    cin >> option;
+    switch (option)
+    {
+        case 1:
+            {
+                manager_portal();
+                return;
+                break;
+            }
+        case 2:
+            {
+                customer();
+                break;
+            }
+        case 3:
+            {
+                /* chef */
+                break;
+            }
+    }
+    return 0;
 }
+
+inline void wrong_entry() // WRONG OPTION ALERT
+{
+    cerr << "\n\n\t\tIncorrect Option! Please choose from the given option.\n\tPress any key to try again...";
+    getch();
+}
+
+inline void title(string t) // TITLE ALIGNEMENT AND DESIGN
+{
+    cout << "\t\t\t***********************************************" << endl;
+    cout << "\t\t\t                " << t << "                        " << endl;
+    cout << "\t\t\t***********************************************" << endl;
+}
+
