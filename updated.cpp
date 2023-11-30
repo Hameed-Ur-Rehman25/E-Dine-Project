a0d0ce0d0e32b7b57b013ebc5603569143b7dee4
Hameed-Ur-Rehman25 <hameedkhan944873@gmail.com>
2023-11-30 14:09:52 +0500

    modified:   updated.cpp

--- updated.cpp
+++ updated.cpp
@@ -8,8 +8,9 @@
 using namespace std;
 
 inline void wrong_entry();
-inline void title();
+inline void title(string t);
 
+void manager_portal();
 
 int main()
 {
@@ -22,22 +23,21 @@ int main()
     cin >> option;
     switch (option)
     {
-        case 1:
-            {
-                manager_portal();
-                return;
-                break;
-            }
-        case 2:
-            {
-                customer();
-                break;
-            }
-        case 3:
-            {
-                /* chef */
-                break;
-            }
+    case 1:
+    {
+        manager_portal();
+        break;
+    }
+    case 2:
+    {
+        // customer();
+        break;
+    }
+    case 3:
+    {
+        /* chef */
+        break;
+    }
     }
     return 0;
 }
@@ -55,3 +55,48 @@ inline void title(string t) // TITLE ALIGNEMENT AND DESIGN
     cout << "\t\t\t***********************************************" << endl;
 }
 
+void manager_portal()
+{
+    int option;
+    system("cls");
+    cout << "\t\t\t\t\t\t\t\t\t*****WELCOME TO MANAGER PORTAL*****\n\n";
+    cout << "\n\t<1> Menu Management\n\n\t<2> Staff\n\n\t<3> Order History\n\n\t<4> Change Password\n\n\t<0> Back To Main Page\n\n\t<00>Exit";
+    while (true)
+    {
+        cout << "\n\n\t\tChoose any one option: ";
+        cin >> option;
+        if (option == 1)
+        {
+            /* code */
+            break;
+        }
+        else if (option == 2)
+        {
+            /* code */
+            break;
+        }
+        else if (option == 3)
+        {
+            /* code */
+            break;
+        }
+        else if (option == 4)
+        {
+            /* code */
+            break;
+        }
+        else if (option == 0)
+        {
+            /* code */
+            break;
+        }
+        else if (option == 00)
+        {
+            exit(0);
+        }
+        else
+        {
+            wrong_entry();
+        }
+    }
+}
